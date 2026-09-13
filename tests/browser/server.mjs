bed1ba840458908a80f2754db4dae4418ee75eaf
Leo Lara <MADPANDA3D@users.noreply.github.com>
2026-09-12 23:28:12 -0400

    chore(release): prepare v1.0.57
    Refs: MAD-943

--- tests/browser/server.mjs
+++ tests/browser/server.mjs
@@ -40,7 +40,7 @@ createServer(async (request, response) => {
     let body = await readFile(asset);
     const cookies = request.headers.cookie?.split(';').map(value => value.trim()) || [];
     if (pathname === '/static/sw.js' && cookies.includes(FUTURE_WORKER_COOKIE)) {
-      body = Buffer.from(body.toString('utf8').replace('pandamonium-v412', 'pandamonium-v413'));
+      body = Buffer.from(body.toString('utf8').replace('pandamonium-v413', 'pandamonium-v414'));
     }
     response.writeHead(200, {
       'Content-Type': CONTENT_TYPES[extname(asset)] || 'application/octet-stream',
